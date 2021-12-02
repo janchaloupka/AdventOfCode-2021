095aa537db15b888660add9b573b18f8043bd7cb
Jan Chaloupka <osobni@janchaloupka.cz>
2021-12-02 19:28:47 +0100

    Den dva jelě lepší oneliner

--- day2/code.mjs
+++ day2/code.mjs
@@ -1,21 +1,8 @@
 import {readFileSync} from "fs";
 
-const lines = readFileSync("input", "utf8").trim().split("\n")
-    .map(v => v
-        .split(" ")
-        .map((v, i) => i > 0 ? parseInt(v, 10) : v)
-    )
-
-// Part 1
-console.log(lines // map => (depth, hor) diff
+console.log(readFileSync("input", "utf8").trim().split("\n")
+    .map(v => v.split(" ").map((v, i) => i > 0 ? parseInt(v, 10) : v))
     .map(v => v[0] == "down" ? [v[1], 0] : v[0] == "up" ? [-v[1], 0] : [0, v[1]])
-    .reduce((p, v) => [p[0] + v[0], p[1] + v[1]], [0, 0])
-    .reduce((p, v) => p * v)
-);
-
-// Part 2
-console.log(lines // map => (aim, hor) diff
-    .map(v => v[0] == "down" ? [v[1], 0] : v[0] == "up" ? [-v[1], 0] : [0, v[1]])
-    .reduce((p, v, i, a) => v[1] == 0 ? p : [p[0] + v[1] * a.slice(0, i).reduce((p, v) => p + v[0], 0), p[1] + v[1]], [0, 0])
-    .reduce((p, v) => p * v)
+    .reduce((p, v, i, a) => v[1] == 0 ? [p[0] + v[0], p[1], p[2], p[3]] : [p[0], p[1] + v[1], p[2] + v[1] * a.slice(0, i).reduce((p, v) => p + v[0], 0), p[3] + v[1]], [0, 0, 0, 0])
+    .reduce((p, v, i) => i < 2 ? [p[0] * v, p[1]] : [p[0], p[1] * v], [1, 1])
 );
