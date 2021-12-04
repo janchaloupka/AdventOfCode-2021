1bac934037575204fce8b032df6a14232d42c3d7
Jan Chaloupka <osobni@janchaloupka.cz>
2021-12-04 13:15:25 +0100

    Den 4 trochu hezčí

--- day4/code.mjs
+++ day4/code.mjs
@@ -11,35 +11,22 @@ const checkWin = card =>
 const applyNumber = (card, number) =>
     card.map(v => v.map(v => v == number ? "*" : v));
 
-const winBingo = (cards, numbers) => {
-    for (const n of numbers) {
+const firstWinBingo = (cards, numbers) =>
+    numbers.map(n => {
         cards = cards.map(card => applyNumber(card, n));
-        for (const card of cards) {
-            if(checkWin(card)){
-                return [card, n];
-            }
-        }
-    }
-}
+        const win = cards.find(v => checkWin(v));
+        if(win) return [win, n];
+    }).find(v => Array.isArray(v));
 
-const lastWinBingo = (cards, numbers) => {
-    for (const n of numbers) {
+const lastWinBingo = (cards, numbers) =>
+    numbers.map(n => {
         cards = cards.map(card => applyNumber(card, n));
-        for (let i=0; i < cards.length; i++) {
-            if(checkWin(cards[i])){
-                if(cards.length === 1){
-                    return [cards[i], n];
-                }else{
-                    cards.splice(i, 1);
-                    i--;
-                }
-            }
-        }
-    }
-}
+        if(cards.length === 1 && checkWin(cards[0])) return [cards[0], n];
+        cards = cards.filter(v => !checkWin(v));
+    }).find(v => Array.isArray(v));
 
-const [winCard, winNumber] = winBingo(cards, numbers);
-console.log(winCard.flat().reduce((p, v) => v == "*" ? p : p + v, 0) * winNumber);
+const [firstWinCard, firstWinNumber] = firstWinBingo(cards, numbers);
+console.log(firstWinCard.flat().reduce((p, v) => v == "*" ? p : p + v, 0) * firstWinNumber);
 
 const [lastWinCard, lastWinNumber] = lastWinBingo(cards, numbers);
 console.log(lastWinCard.flat().reduce((p, v) => v == "*" ? p : p + v, 0) * lastWinNumber);
